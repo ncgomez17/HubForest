166540032126c72ff542b3822f7f58323c373421
Nicolás Cid Gómez <ncgomez17@esei.uvigo.es>
2023-12-27 01:33:31 +0100

    creado metodos en el front de search_by y edit

--- REST/HubForestView/js_app/usuario/usuario.js
+++ REST/HubForestView/js_app/usuario/usuario.js
@@ -6,6 +6,20 @@ function getListUsuarios() {
             return null;
         });
 }
+function getListByParamUsuarios(nombre, correo, password, rol) {
+    const usuario = {
+        nombre: nombre,
+        correo: correo,
+        password: password,
+        rol: rol
+    };
+    return peticionBackGeneral('', 'usuario', 'SEARCH_BY', usuario)
+        .then(response => (response['code'] === 'RECORDSET_DATOS') ? response['resource'] : null)
+        .catch(error => {
+            console.error('Error en la solicitud:', error);
+            return null;
+        });
+}
 
 function addUsuario(nombre, correo, password, rol) {
     const usuario = {
@@ -14,8 +28,30 @@ function addUsuario(nombre, correo, password, rol) {
         password: password,
         rol: rol
     };
+
     return peticionBackGeneral('', 'usuario', 'ADD', usuario)
-        .then(response => console.log("Solicitud con éxito",response))
+        .then(response => {
+            return { status: 'OK', data: response };
+        })
+        .catch(error => {
+            console.error('Error en la solicitud:', error);
+            return null;
+        });
+}
+
+function editUsuario(idUsuario,nombre, correo, password, rol) {
+    const usuario = {
+        id: idUsuario,
+        nombre: nombre,
+        correo: correo,
+        password: password,
+        rol: rol
+    };
+
+    return peticionBackGeneral('', 'usuario', 'EDIT', usuario)
+        .then(response => {
+            return { status: 'OK', data: response };
+        })
         .catch(error => {
             console.error('Error en la solicitud:', error);
             return null;
@@ -23,8 +59,10 @@ function addUsuario(nombre, correo, password, rol) {
 }
 
 function deleteUsuario(idUsuario) {
-    return peticionBackGeneral('', 'usuario', 'DELETE', {'id':idUsuario})
-        .then(response => console.log("Solicitud con éxito",response))
+    return peticionBackGeneral('', 'usuario', 'DELETE', {'id': idUsuario})
+        .then(response => {
+            return { status: 'OK', data: response };
+        })
         .catch(error => {
             console.error('Error en la solicitud:', error);
             return null;
